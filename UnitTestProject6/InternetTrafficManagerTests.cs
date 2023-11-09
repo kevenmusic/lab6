6cfc2efb0fbd4b3abd76eb15feca9e7ea07357d1
Константин <@HOME-PC>
2023-11-09 21:20:08 +0300

    Добавил модульные тесты для класса InternetTrafficManager

--- UnitTestProject6/InternetTrafficManagerTests.cs
+++ UnitTestProject6/InternetTrafficManagerTests.cs
@@ -45,5 +45,71 @@ namespace UnitTestProject6
             // Assert
             Assert.AreEqual(125, result);
         }
+
+        [TestMethod]
+        public void TestDayWithMaxTraffic()
+        {
+            // Arrange
+            InternetTrafficManager.InternetTraffic[] testData = new InternetTrafficManager.InternetTraffic[]
+            {
+                new InternetTrafficManager.InternetTraffic(new DateTime(2023, 11, 5), InternetTrafficManager.ProtocolType.HTTP, 100),
+                new InternetTrafficManager.InternetTraffic(new DateTime(2023, 11, 6), InternetTrafficManager.ProtocolType.HTTP, 150),
+                new InternetTrafficManager.InternetTraffic(new DateTime(2023, 11, 6), InternetTrafficManager.ProtocolType.FTP, 200)
+            };
+            TrafficAnalyzer analyzer = new TrafficAnalyzer(testData);
+
+            // Act
+            DateTime result = analyzer.DayWithMaxTraffic(InternetTrafficManager.ProtocolType.HTTP);
+
+            // Assert
+            Assert.AreEqual(new DateTime(2023, 11, 6), result);
+        }
+
+        [TestMethod]
+        public void TestTotalTrafficOnNonexistentDate()
+        {
+            // Arrange
+            InternetTrafficManager.InternetTraffic[] testData = new InternetTrafficManager.InternetTraffic[]
+            {
+                new InternetTrafficManager.InternetTraffic(new DateTime(2023, 11, 5), InternetTrafficManager.ProtocolType.HTTP, 100),
+                new InternetTrafficManager.InternetTraffic(new DateTime(2023, 11, 6), InternetTrafficManager.ProtocolType.HTTP, 150),
+                new InternetTrafficManager.InternetTraffic(new DateTime(2023, 11, 6), InternetTrafficManager.ProtocolType.FTP, 200)
+            };
+            TrafficAnalyzer analyzer = new TrafficAnalyzer(testData);
+
+            // Act
+            double result = analyzer.TotalTrafficOnDate(new DateTime(2023, 11, 7));
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestAverageTrafficPerDayWithNoData()
+        {
+            // Arrange
+            InternetTrafficManager.InternetTraffic[] testData = new InternetTrafficManager.InternetTraffic[] { };
+            TrafficAnalyzer analyzer = new TrafficAnalyzer(testData);
+
+            // Act
+            double result = analyzer.AverageTrafficPerDay(InternetTrafficManager.ProtocolType.HTTP, new DateTime(2023, 11, 5), new DateTime(2023, 11, 6));
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestDayWithMaxTrafficWithNoData()
+        {
+            // Arrange
+            InternetTrafficManager.InternetTraffic[] testData = new InternetTrafficManager.InternetTraffic[] { };
+            TrafficAnalyzer analyzer = new TrafficAnalyzer(testData);
+
+            // Act
+            DateTime result = analyzer.DayWithMaxTraffic(InternetTrafficManager.ProtocolType.HTTP);
+
+            // Assert
+            Assert.AreEqual(DateTime.MinValue, result);
+        }
     }
 }
